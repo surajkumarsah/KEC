f464c291ef929f6346403e07b19f5d74e05d4b7b
Suraj kumar <surajkumarsah165@gmail.com>
2019-08-28 23:01:12 +0530

    28/aug/2019 file semester updated

--- app/src/main/java/com/example/KEC/Semester_Activity.java
+++ app/src/main/java/com/example/KEC/Semester_Activity.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 
 public class Semester_Activity extends AppCompatActivity {
 
-    private ImageView Semester1, Semester2;
+    private ImageView Semester1, Semester2,Semester3,Semester4,Semester5,Semester6,Semester7,Semester8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,12 @@ public class Semester_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_semester_);
 
         Semester1 = (ImageView) findViewById(R.id.sem1);
+        Semester3 = (ImageView) findViewById(R.id.sem3);
+        Semester4 = (ImageView) findViewById(R.id.sem4);
+        Semester5 = (ImageView) findViewById(R.id.sem1);
+        Semester6 = (ImageView) findViewById(R.id.sem1);
+        Semester7 = (ImageView) findViewById(R.id.sem1);
+        Semester8 = (ImageView) findViewById(R.id.sem1);
 
         Semester1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,7 +39,61 @@ public class Semester_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                Intent intent = new Intent(Semester_Activity.this,MainActivity.class);
+                Intent intent = new Intent(Semester_Activity.this,Syll_Sem2_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Semester3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Semester_Activity.this,Syll_Sem3_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Semester4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Semester_Activity.this,Syll_Sem4_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Semester5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Semester_Activity.this,Syll_Sem5_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Semester6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Semester_Activity.this,Syll_Sem6_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Semester7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Semester_Activity.this,Syll_Sem7_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Semester8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Semester_Activity.this,Syll_Sem7_Activity.class);
                 startActivity(intent);
             }
         });
