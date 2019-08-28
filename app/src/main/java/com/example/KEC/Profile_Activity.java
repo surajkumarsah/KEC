d5ee38f3b0a6a5ddb169b3e6947ab644a979db52
Suraj kumar <surajkumarsah165@gmail.com>
2019-08-28 22:15:35 +0530

    28/aug/2019 file updated

--- app/src/main/java/com/example/KEC/Profile_Activity.java
+++ app/src/main/java/com/example/KEC/Profile_Activity.java
@@ -2,13 +2,36 @@ package com.example.KEC;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Profile_Activity extends AppCompatActivity {
 
+    private Button studadd,SemesterBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_);
+
+        studadd = (Button) findViewById(R.id.addstud);
+        SemesterBtn = (Button) findViewById(R.id.sem_btn);
+
+        studadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile_Activity.this,Add_Stud_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        SemesterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile_Activity.this,Semester_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
